ef704f6eec9730d23cd347a8b45fc1ee4890c846
Breckfab <151375745+Breckfab@users.noreply.github.com>
2026-07-07 19:43:24 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function sendMessage() {
-    const input = document.getElementById('user-input');
-    const message = input.value;
-    const tinaDisplay = document.getElementById('tina-message');
-    
-    // Lógica temporal de inicio
-    tinaDisplay.innerText = "Hello! I am Tina, your English teacher. To start our first lesson, what is your name?";
-    input.value = "";
-}
